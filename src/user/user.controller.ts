09aa68780d9fbf36de90760117125fcc181c18a7
Bogdan Migunov <migunov.bbbb@gmail.com>
2023-11-02 18:37:41 +0300

    add user delete feature and fix schema

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,6 +9,8 @@ import {
 } from '@nestjs/common';
 import { UserService } from '@user/user.service';
 import { UserResponse } from '@user/responses';
+import { CurrentUser } from '@common/decorators';
+import { JwtPayload } from '@auth/interfaces';
 
 @Controller('user')
 export class UserController {
@@ -21,7 +23,7 @@ export class UserController {
   }
 
   @Delete(':id')
-  deleteUser(@Param('id', ParseUUIDPipe) id: string) {
-    return this.userService.delete(id);
+  deleteUser(@Param('id', ParseUUIDPipe) id: string, @CurrentUser() user: JwtPayload) {
+    return this.userService.delete(id, user);
   }
 }
