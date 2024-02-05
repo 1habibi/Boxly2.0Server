6178b1b430d9a2385491669d469040a7ef470f8f
Bogdan Migunov <migunov.bbbb@gmail.com>
2024-02-05 13:14:28 +0300

    fix: fix CORS and sameSite cookies for develop

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
