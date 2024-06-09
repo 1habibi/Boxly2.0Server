e93dfe8d8fe0180c33c89d9d7b09e406be0a7387
Bogdan Migunov <migunov.bbbb@gmail.com>
2024-06-09 19:37:18 +0300

    feat: update cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://ik.imagekit.io', 'http://192.168.0.106:8081'],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
